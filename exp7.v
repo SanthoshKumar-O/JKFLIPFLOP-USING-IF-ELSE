c8af2f23d299357bcaadb254ca205d5852e2f339
SanthoshKumar-O <santhoshkumar19d07@gmail.com>
2025-12-19 11:36:06 +0530

    Add files via upload

---
+++ exp7.v
@@ -0,0 +1,17 @@
+module exp7(j, k, clk, rst, q);
+input j, k, clk, rst;
+output reg q;
+always @(posedge clk or posedge rst) 
+begin
+if (rst)
+q <= 0;
+else if (j == 0 && k == 0)
+q <= q;
+else if (j == 0 && k == 1)
+q <= 0;
+else if (j == 1 && k == 0)
+q <= 1;
+else if (j == 1 && k == 1)
+q <= ~q;
+end
+endmodule 
